02b0c12446dfea5c1e79439d2ef8e4c9b6969af8
James McLain <shakepeare@sbcglobal.net>
2023-08-26 02:48:55 -0700

    Add files via upload
    Made a basic Restaurant menu static page, following up to time 52:55 in the video. Added a styled black back ground, and bluish text, photos of menu items were created in MidJourney.

---
+++ index.js
@@ -0,0 +1,18 @@
+const navbar = (
+<nav>
+    <h1>James McLain</h1>
+    <h3>My Art and Designs</h3>
+    <ol>
+        <li><p>Ribs</p><img src='.\ribs.png' alt="a picture of ribs"></img></li>
+        <li><p>Cold Slaw</p><img src='.\cold-slaw.png' alt="a picture of cold slaw"></img></li>
+        <li><p>Potato Salad</p><img src='.\potato-salad.png' alt="a picture of potato salad"></img></li>
+        
+    </ol>
+
+    </nav>
+)
+
+
+
+ReactDOM.render(navbar , document.getElementById("root"))
+
